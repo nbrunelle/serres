62d5acdc54ce9873315b03d238a0edca60fffb97
name <mail@nicolasbrunelle.com>
2025-01-19 10:46:39 +0100

    update

--- index.php
+++ index.php
@@ -5,8 +5,115 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serres*</title>
+    <link rel="stylesheet" href="styles/stylesheet.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
+    <style>
+        body,
+        html{
+            margin:0;
+            padding:0;
+            font-family: glacial_indifferenceregular;
+            text-align: center;
+            margin: 5vw;
+            margin-top: 2vw;
+            max-width: 90vw;
+        }
+        @media (width > 720px) {
+            body,html{
+                max-width: 80%;
+                margin: 10%; 
+                margin-top: 2vw;
+            }
+        }
+        img{
+            max-width:100%; 
+        }
+        h1{
+            padding:0;
+            margin:0;
+            /* font-family: glacial_indifferencebold; */
+        }
+        .ital{
+            font-family: glacial_indifferenceitalic;
+        }
+        .bold{
+            font-family: glacial_indifferencebold;
+        }
+        #logo{
+            max-width: 20vw;
+            padding-left: 2%;
+        }
+        p{
+            text-justify: distribute;
+            text-align: justify;
+            line-height: 1.3;
+            padding-bottom: 10px;
+        }
+        .blog{
+            margin-bottom:15px;
+        }
+        .footer {
+            padding-top: 100px;
+        }
+    </style>
 </head>
 <body>
-    Coming soon…
-</body>
+    <div class="head">
+        <h1>Serres*</h1>
+        <img id="logo" src="img/serres.svg" alt="" srcset="">
+        <p>Transdisciplinary mensual bilingual editon <br>Revue transdisciplinaire mensuelle bilingue</p>
+        <p>Serres* comes together as a few pages to invest in where <em>multiple rhythms</em>, forms and angles collide. A project for which its frame is diaphanous, a space for experimentation, fragile tests, beginnings, intuitions. Serres is the desire to share these thoughts, to give them shape, give them space, to make them relate in the in between of stages of a process. <br> <br>
+        L’opération de mise en forme nous intéresse ; celle des idées, des impressions, des gestes, qui se fait à chaque instant de la vie et qui trouve dans les œuvres une intensité renouvellée. Il s’agit d’en proposer des figures, à partir de divers points de vue par lesquels entrevoir la réalité : arts visuels, philosophie, sciences, poésie, savoirs-faire et plus encore. Dans les édifices théoriques, esthétiques, imaginaires, nous pensons trouver une fenêtre sur le monde, à travers laquelle il se trouve d'un même acte présenté et transformé. Alors s'érigent, ravivées, notre attention et notre attente.
+        </p>
+        <p>
+            instagram : <a target="_blank" href="https://www.instagram.com/serres_revue/">@serres_revue</a>
+        </p>
+    </div>
+    <div class="blog">
+        <span class="ital">
+            pre-command the next edition,
+            prix libre. prix recommandé: 5€
+        </span> <br><br>
+        <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/entropies/boutiques/serres-1/widget-bouton" style="height: 70px; border: none;"></iframe>
+    </div>
+
+
+    <!-- SERRES* #1 !-->
+    <div class="blog">
+        <h3>#1 – march 2023</h3>
+        <img src="img/upload/2.jpg" alt="">
+        <p>with the contributions of :
+            <ul style="list-style-type:none;text-align:left;">
+                <li>Lucie (<a href="https://www.instagram.com/nuktos_hemera/" target="_blank" rel="noopener noreferrer">@nuktos_hemera</a>)</li>
+                <li>Sara Tremblay (<a href="https://www.instagram.com/saramenedutiff/" target="_blank" rel="noopener noreferrer">@saramenedutiff</a>)</li>
+                <li>Sasha Lithrue (<a href="https://www.instagram.com/sashalithrue/" target="_blank" rel="noopener noreferrer">@sashalithrue</a>)</li>
+                <li>Amandine (<a href="https://www.instagram.com/amandineteulier/" target="_blank" rel="noopener noreferrer">@amandineteulier</a>)</li>
+                <li>Francis Sandmann (<a href="https://www.instagram.com/exmackinaw/" target="_blank" rel="noopener noreferrer">@exmackinaw</a>)</li>
+                <li>Martin (<a href="https://www.instagram.com/meussieu_martin/" target="_blank" rel="noopener noreferrer">@meussieu_martin</a>)</li>
+                <li>Mzioff (<a href="https://www.instagram.com/_ntmzi/" target="_blank" rel="noopener noreferrer">@_ntmzi</a>)</li>
+            </ul>
+        </p>
+        Thanks Nathalie Spaudling (<a href="https://www.instagram.com/echec.et.nat/" target="_blank" rel="noopener noreferrer">@echec.et.nat</a>) for the help with the translation and Valentine Iglicki (<a href="https://www.instagram.com/_vigk_/" target="_blank" rel="noopener noreferrer">@_vigk_</a>) for the graphic composition. 
+
+    </div>
+
+    <!-- Analogies !-->
+    <div class="blog">
+        <h3>Analogies</h3>
+
+        <img src="https://nicolasbrunelle.com/cp/assets/link/6cb00ba5326462e80900038f" alt="">
+
+
+    </div>
+
+    <!-- Analogies !-->
+    <div class="blog">
+        <h3>#2</h3>
+        <i>Coming up</i>
+    </div>
+
+    <div class="footer">
+        Serres* is a project of Entropies <br> with collaboration of <a href="http://cobyrae.com">Coby-Rae Crosbie</a> and Lucas Berthevas
+    </div>
+    </body>
 </html>
